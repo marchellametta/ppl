338b768236ebc28c268e404c93660a1d3144a6c3
Elia March <eliamarch@Elias-MacBook-Pro.local>
2017-11-20 12:16:49 +0700

    update invitationControllerTest

--- Source/src/test/java/com/mscheduler/controller/InvitationControllerTest.java
+++ Source/src/test/java/com/mscheduler/controller/InvitationControllerTest.java
@@ -11,6 +11,10 @@ import com.mscheduler.model.Invitation;
 import com.mscheduler.model.ListInvitationViewModel;
 import java.util.ArrayList;
 import java.util.List;
+import org.easymock.EasyMock;
+import org.easymock.EasyMockRule;
+import org.easymock.Mock;
+import org.easymock.TestSubject;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -23,6 +27,14 @@ import static org.junit.Assert.*;
  * @author Clara Christina
  */
 public class InvitationControllerTest {
+    
+    public EasyMockRule rule = new EasyMockRule(this);
+    @Mock
+    private InvitationController ic =  EasyMock.createMock(InvitationController.class); // 1
+    private SchedulerController sc =  EasyMock.createMock(SchedulerController.class);
+    
+    @TestSubject
+    private MeetingController mc; // 2
     
     public InvitationControllerTest() {
     }
@@ -52,8 +64,6 @@ public class InvitationControllerTest {
         InvitationController expResult = InvitationController.getInstance();
         InvitationController result = InvitationController.getInstance();
         assertEquals(expResult, result);
-
-        
     }
 
     /**
@@ -66,8 +76,6 @@ public class InvitationControllerTest {
         List<ListInvitationViewModel> expResult = instance.listInvitationView();
         List<ListInvitationViewModel> result = instance.listInvitationView();
         assertEquals(expResult, result);
-   
-        
     }
 
     /**
