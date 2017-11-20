db8fb01f73a244ce819f6d02c37162ce447c8741
marchmetta <morningracoon@gmail.com>
2017-11-20 20:16:53 +0700

    perbaikan tanggal

--- Source/src/test/java/com/mscheduler/model/InvitationTest.java
+++ Source/src/test/java/com/mscheduler/model/InvitationTest.java
@@ -122,7 +122,7 @@ public class InvitationTest {
     public void testSetAvailability() {
         System.out.println("setAvailability");
         List<DateRange> availability = new ArrayList<DateRange>();
-        availability.add(new DateRange("01-01-2017 - 01-06-2017"));
+        availability.add(new DateRange("01/01/2017 - 01/06/2017"));
         Invitation instance = new Invitation();
         instance.setAvailability(availability);
         List<DateRange> result = instance.getAvailability();
@@ -139,7 +139,7 @@ public class InvitationTest {
     @Test
     public void testAddAvailability() {
         System.out.println("addAvailability");
-        DateRange newDateRange = new DateRange("02-06-2017 - 31-12-2017");
+        DateRange newDateRange = new DateRange("02/06/2017 - 31/12/2017");
         Invitation instance = new Invitation();
         List<DateRange> expectedResult = instance.getAvailability();
         expectedResult.add(newDateRange);
